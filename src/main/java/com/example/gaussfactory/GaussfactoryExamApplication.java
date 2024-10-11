4bb6bf675784e23e65c2acdaa899e2cb9f074a3a
Tapiiaa <ptapilob@myuax.com>
2024-10-11 16:22:06 +0200

    Initial commit

---
+++ src/main/java/com/example/gaussfactory/GaussfactoryExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaussfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaussfactoryExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaussfactoryExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gaussfactory/GaussfactoryExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gaussfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaussfactoryExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
